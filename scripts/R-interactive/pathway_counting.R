a1a51d95af7628911291134c2896ea708b24fa13
Scott Daniel <scottdaniel25@gmail.com>
2018-01-31 16:34:53 -0700

    brain is starting to feel mushy

--- scripts/R-interactive/pathway_counting.R
+++ scripts/R-interactive/pathway_counting.R
@@ -1,5 +1,6 @@
 setwd("~/isogenie-assemblies/prokka_annotations")
-
+library("plyr")
+library("dplyr")
 temp = list.files(recursive=TRUE,pattern="*.tsv")
 myfiles = lapply(temp, read.delim)
 sample_names <- as.list(sub('\\/PROKKA.*tsv', "", temp, perl=T))
@@ -13,3 +14,22 @@ onlyEC = function(df) {
 }
 
 finally = Map(onlyEC,myfiles)
+df <- ldply(finally, data.frame)
+
+groupies <- group_by(df, sample, EC_number)
+ECcounts <- summarise(groupies, EC_count=n())
+
+#what's in 1 but not in 2
+setdiff(finally[[1]]$EC_number,finally[[2]]$EC_number)
+
+which(finally[[1]]$EC_number=="4.1.99.14")
+which(finally[[2]]$EC_number=="4.1.99.14")
+
+length(which(ECcounts$sample=="Day0_Erio"))
+#2048
+length(which(ECcounts$sample=="Day50_Erio"))
+#1874
+length(which(ECcounts$sample=="Day0_SPH"))
+#1978
+length(which(ECcounts$sample=="Day50_SPH"))
+#1977
